4bb43f115e72aaa02e18ed834fc7c6bebc211cfd
neal <neal.gpt@gmail.com>
2014-01-10 22:28:56 +0530

    the first commit on sushma file

--- fd.py
+++ fd.py
@@ -1,8 +1,8 @@
 def function(str1,str2):
         for i in range(0,len(str1),1):
                 if str1[i]==str2[i]:
-                        s=str1
-                        print s
+                        s1=str1
+                        print s1
                 else:
                         print 'not found'
 
