de26b04803ac63ed004bd6a95d0409749b9246be
saisushma <smsaisushma.gmail.com>
2014-01-10 21:46:32 +0530

    this is first commit on the clone

---
+++ fd.py
@@ -0,0 +1,9 @@
+def function(str1,str2):
+        for i in range(0,len(str1),1):
+                if str1[i]==str2[i]:
+                        s=str1
+                        print s
+                else:
+                        print 'not found'
+
+
